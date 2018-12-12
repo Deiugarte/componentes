57c5ed2b91a3aa45b46dffb65274c51aab3183db
segura27 <segura2794@gmail.com>
2018-12-12 00:04:18 -0600

    Start

--- src/main/java/edu/ulatina/controller/LoginController.java
+++ src/main/java/edu/ulatina/controller/LoginController.java
@@ -1,15 +1,9 @@
 package edu.ulatina.controller;
 
 import java.io.Serializable;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+
 
 @ManagedBean(name = "loginController")
 @SessionScoped
@@ -30,11 +24,18 @@ public class LoginController implements Serializable {
     private boolean logeado = false;
 
     private int intentos = 0; // para bloquear cuenta...
-
     private String user, pass;
     private String nombre, ape, ape2, naci, sexo, tel;
     private boolean estado = false;
-
+    
+    public void submit() {
+    System.out.println("*NOMBRE*: " + nombre);
+}
+    public void userIsRegistered(){
+        //CHECK AGAINST DB IF EMAIL ADDRESS IS REGISTERED
+        
+        /*IF IT IS REGISTERED THEN ALLOW ACCESS, OTHERWISE REDIRECT TO SIGNUP PAGE!*/
+    }
     public void getDataFromResponse(String response) {
         System.out.println(response);
     }
@@ -98,5 +99,14 @@ public class LoginController implements Serializable {
     public void setCode(String code) {
         this.code = code;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 
 }
